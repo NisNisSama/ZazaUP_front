d2f7cb4b02df733b4be8212dea139478480b5830
NisNisSama <rhn819@gmail.com>
2023-01-27 17:09:38 +0300

    Version 0.2

---
+++ zazaUP/src/app/playground/components/decouverte/decouverte.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-decouverte',
+  templateUrl: './decouverte.component.html',
+  styleUrls: ['./decouverte.component.scss']
+})
+export class DecouverteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ zazaUP/src/app/playground/playground.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlaygroundRoutingModule } from './playground-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DecouverteComponent } from './components/decouverte/decouverte.component';
+import { ApprendreComponent } from './components/apprendre/apprendre.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+import { QuestionComponent } from './components/question/question.component';
+import { ChatComponent } from './components/chat/chat.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    SidenavComponent,
+    HeaderComponent,
+    DecouverteComponent,
+    ApprendreComponent,
+    QuizzComponent,
+    QuestionComponent,
+    ChatComponent
+  ],
+  imports: [
+    CommonModule,
+    PlaygroundRoutingModule
+  ]
+})
+export class PlaygroundModule { }
